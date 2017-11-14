67b957d98af9dd628155f7b1193f170e555a54fe
Marco Herglotz <herglotz.m@gmail.com>
2017-11-14 10:59:53 +0100

    Implemented drag and drop with listener pattern

--- drag-and-drop/src/main/java/de/tinf15b4/frp/paradigms/ListenerPattern.java
+++ drag-and-drop/src/main/java/de/tinf15b4/frp/paradigms/ListenerPattern.java
@@ -1,21 +1,59 @@
 package de.tinf15b4.frp.paradigms;
 
+import java.awt.Point;
+import java.util.Optional;
+import java.util.UUID;
+
 import de.tinf15b4.frp.api.Document;
+import de.tinf15b4.frp.api.Document.Entry;
+import de.tinf15b4.frp.api.Element;
 import de.tinf15b4.frp.api.MouseEvent;
 import de.tinf15b4.frp.api.Paradigm;
 
 public class ListenerPattern implements Paradigm {
 
-    private final DocumentListener documentListener;
-    private Document document;
+	private final DocumentListener documentListener;
+	private Document document;
 
-    public ListenerPattern(Document initDoc,  DocumentListener documentListener) {
-        this.document = initDoc;
-        this.documentListener = documentListener;
-    }
+	private UUID toDragId;
+	private Element toDrag;
+	private Point lastPoint;
 
-    public void mouseEvent(MouseEvent mouseEvent) {
-    }
+	public ListenerPattern(Document initDoc, DocumentListener documentListener) {
+		this.document = initDoc;
+		this.documentListener = documentListener;
+	}
 
-    public void dispose() {}
+	@Override
+	public void mouseEvent(MouseEvent mouseEvent) {
+		switch (mouseEvent.type()) {
+		case DOWN:
+			Optional<? extends Entry> entry = document.atPoint(mouseEvent.point());
+			entry.ifPresent(e -> {
+				toDrag = e.element();
+				toDragId = e.id();
+				lastPoint = mouseEvent.point();
+			});
+			break;
+		case UP:
+			toDrag = null;
+			toDragId = null;
+			lastPoint = null;
+			break;
+		case MOVE:
+			if (toDrag != null && toDragId != null) {
+				Point translation = new Point(mouseEvent.point().x - lastPoint.x, mouseEvent.point().y - lastPoint.y);
+				lastPoint = mouseEvent.point();
+				Element dragged = toDrag.translate(translation);
+				document = document.insert(toDragId, dragged);
+				toDrag = dragged;
+				documentListener.documentUpdated(document);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void dispose() {
+	}
 }
